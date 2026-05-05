ace9f26a3451f7dcf8d82348b7906d9c10515211
faisal ali zaid <zaidalideveloper@gmail.com>
2026-05-05 14:34:00 +0300

    added animation effect

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let list = document.querySelectorAll('.carousel .listn.item');
+let list = document.querySelectorAll('.carousel .list .item');
 let carousel=document.querySelector('.carousel');
 let dots=document.querySelectorAll('.dots li');
 let nextBtn=document.getElementById('next');
 let prevBtn=document.getElementById('prev');
 
-let lastPosition= list.lenth-1;
+let lastPosition= list.length-1;
 let active=0;
 let zIndex=2;
 
@@ -16,7 +16,34 @@ prevBtn.onclick=()=>{
     let newValue =active -1<0? lastPosition : active - 1;
     setItemActive(newValue, showSlider);
 }
-const showSlider =()=>{
+const setItemActive = (newValue, callbackFunction)=>{
+    if(newValue === active) return;
+    let type = newValue>active?'next':'prev';
+    active=newValue;
+    callbackFunction(type);
+}
+let removeEffect;
+const showSlider =(type)=>{
+    //find item active old
+    let itemActiveOld=document.querySelector('.carousel .list .item.active')
+    if(itemActiveOld) itemActiveOld.classList.remove('active');
+    zIndex++;
+    list[active].style.zIndex=zIndex;
+    list[active].classList.add('active');
+
+    carousel.classList.add('effect');
+
+    //dots
+    //find dot active old
+    let dotActiveOld=document.querySelector('dots li,active');
+    if(dotActiveOld) dotActiveOld.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearTimeout(removeEffect);
+    removeEffect=setTimeout(()=>{
+        carousel.classList.remove('effect');
+
+    },1500);
 
 }
 
